fc6cbb37f9ae681655d7503039e50f892da47cdb
alexxxmf <amoro@alumni.unav.es>
2017-11-04 10:44:25 +0000

    Some cleanup in models

--- referral_app/subscribers/admin.py
+++ referral_app/subscribers/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import Subscriber
 
-admin.site.register(Subscriber)
+
+class SubscriberAdmin(admin.ModelAdmin):
+
+    list_display = (
+    	'email',
+    	'email_from_referrer',
+    	'confirmed_subscription',
+    	'ip',
+    	'referral_count',
+    	'referred',
+    	'unique_code',
+    )
+
+
+admin.site.register(Subscriber, SubscriberAdmin)
--- referral_app/subscribers/models.py
+++ referral_app/subscribers/models.py
@@ -30,16 +30,3 @@ class Subscriber(models.Model):
 
     def __str__(self):
         return self.email
-
-
-class Reward(models.Model):
-    db_table = 'rewards'
-
-    title = models.CharField(max_length=40, default='')
-    description = models.CharField(max_length=196, default='')
-    image = models.CharField(max_length=196, default='')
-    required_referrals = models.IntegerField(default=0)
-    live = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
